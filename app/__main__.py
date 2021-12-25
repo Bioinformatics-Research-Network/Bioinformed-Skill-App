56e16ec85ae625549e1c782f1530c6f384dce1ef
julian-borbeck <julian.borbeck@gmail.com>
2021-12-25 15:17:25 +0100

    added comment functionality as test

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,39 @@
 import os
+import aiohttp
 
 from aiohttp import web
 
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
 routes = web.RouteTableDef()
 
-@routes.get("/")
+router = routing.Router()
+
+@router.register("issues", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["issue"]["comments_url"]
+    author = event.data["issue"]["user"]["login"]
+
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."
+    await gh.post(url, data={"body": message})
+
+@routes.post("/")
 async def main(request):
-    return web.Response(status=200, text="Hello world!")
+    body = await request.read()
+
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "mariatta",
+                                  oauth_token=oauth_token)
+        await router.dispatch(event, gh)
+    return web.Response(status=200)
 
 
 if __name__ == "__main__":

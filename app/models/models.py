7e68ef2f856a0e528bc59a3064f7e09d7ef0510c
Anmol Singh <broanmol17@gmail.com>
2022-03-10 12:52:18 +0000

    models updated

--- app/models/models.py
+++ app/models/models.py
@@ -1,32 +1,43 @@
-from sqlalchemy import DateTime, JSON, String, Integer, Column, ForeignKey
+from sqlalchemy import ARRAY, DateTime, JSON, String, Integer, Column, ForeignKey
 from sqlalchemy.orm import relationship
 from db.base_class import Base
 
 class Users(Base):
+    """
+    Shared with Airtable
+    """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, unique=True, index=True)
     github_username = Column(String, unique=True, index=True)
-    first_name = Column(String, index=True)
-    last_name = Column(String, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
     email = Column(String, unique=True, index=True)
+    assessments_completed_id = relationship("Assessments") # ID of assessments completed
+    ongoing_assessments = relationship("Assessment_Tracker", back_populates="user")
 
 class Reviewers(Base):
     __tablename__ = "reviewers"
     id = Column(Integer, primary_key=True, unique=True, index=True)
     user = relationship("User")
 
+
 class Assessment_Tracker(Base):
     __tablename__ = "assessment_tracker"
     entry_id = Column(Integer, primary_key=True, unique=True, index=True)
-    user = relationship("User")
-    assessment_id= Column(Integer, ForeignKey("assessments.id"))
+    user = relationship("User", back_populates="ongoing_assessments")
+    assessment_id = Column(Integer, ForeignKey("assessments.id"))
     status = Column(String)
     last_updated = Column(DateTime)
     latest_commit = Column(String, nullable=False, unique=True)
     reviewer = relationship("Reviewers")
-    # Log    # Working on it
-    #     JSON containing full log of review process, including timestamps
+    log = Column(JSON, nullable=False)
 
-class Assessments(Base): # working on it
+class Assessments(Base): 
     __tablename__ = "assessments"
     id = Column(Integer, primary_key=True, unique=True, index=True)
+    name = Column(String)
+    version_number = Column(String)
+    change_log = Column(JSON)
+    description = Column(String)
+    pre_requisites = relationship("Assessments")
+    goals = Column(String)

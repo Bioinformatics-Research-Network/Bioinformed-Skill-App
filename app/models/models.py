6865b8e08cbf25a16dafc6160b3b33f2b9aa59cf
Anmol Singh <broanmol17@gmail.com>
2022-03-10 08:39:47 +0000

    database models: users and reviewers v.1

---
+++ app/models/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import DateTime, JSON, String, Integer, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from db.base_class import Base
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, unique=True, index=True)
+    github_username = Column(String, unique=True, index=True)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+
+class Reviewers(Base):
+    __tablename__ = "reviewers"
+    id = Column(Integer, primary_key=True, unique=True, index=True)
+    user = relationship("User")
+
+class Assessment_Tracker(Base):
+    __tablename__ = "assessment_tracker"
+    entry_id = Column(Integer, primary_key=True, unique=True, index=True)
+    user = relationship("User")
+    assessment_id= Column(Integer, ForeignKey("assessments.id"))
+    status = Column(String)
+    last_updated = Column(DateTime)
+    latest_commit = Column(String, nullable=False, unique=True)
+    reviewer = relationship("Reviewers")
+    # Log
+    #     JSON containing full log of review process, including timestamps
+
+class Assessments(Base):
+    __tablename__ = "assessments"
+    id = Column(Integer, primary_key=True, unique=True, index=True)

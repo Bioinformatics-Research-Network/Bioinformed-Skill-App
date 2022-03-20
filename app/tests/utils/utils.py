c505059805ab51560186b9f0ca23ab27f7ef78f0
Anmol Singh <broanmol17@gmail.com>
2022-03-20 14:36:57 +0000

    fake users added test1

--- app/tests/utils/utils.py
+++ app/tests/utils/utils.py
@@ -1,37 +1,38 @@
 import random
 
 # lists to create random names
-first_list=['Alison' 'Ashley' 'Barbara' 'Benjamin' 'Bernice' 'Betsy' 'Bonnie'
-                'Brandon' 'Brian' 'Brittany' 'Candy' 'Catherine' 'Cheryl' 'Christina'
-                'Clara' 'Clarence' 'Cythia' 'Diane' 'Dianne' 'Donna' 'Edward' 'Gary'
-                'Gladys' 'Gudrun' 'Hazel' 'Henry' 'Hugo' 'Jamie' 'Jay' 'Jill' 'John'
-                'Katherine' 'Kellie' 'Kelly' 'Ken' 'Leon' 'Lillie' 'Lori' 'Luke' 'Marion'
-                'Michell' 'Nicole' 'Robert' 'Robin' 'Ruth' 'Stanley' 'Steven' 'Thomas'
+first_list=['Alison', 'Ashley', 'Barbara', 'Benjamin', 'Bernice', 'Betsy', 'Bonnie',
+                'Brandon', 'Brian', 'Brittany', 'Candy', 'Catherine', 'Cheryl', 'Christina',
+                'Clara', 'Clarence', 'Cythia', 'Diane', 'Dianne', 'Donna', 'Edward', 'Gary',
+                'Gladys', 'Gudrun', 'Hazel', 'Henry', 'Hugo', 'Jamie' 'Jay', 'Jill', 'John',
+                'Katherine', 'Kell,ie', 'Kelly', 'Ken', 'Leon', 'Lillie', 'Lori', 'Luke', 'Marion',
+                'Michell', 'Nicole', 'Robert', 'Robin', 'Ruth', 'Stanley', 'Steven', 'Thomas',
                 'Toni']
     
-last_list=['Baylock' 'Benites' 'Blevins' 'Bossert' 'Bowen' 'Campbell' 'Cartwright'
-                'Cayer' 'Corradino' 'Desrochers' 'Doggett' 'Drain' 'Elizondo' 'Enos'
-                'Gardner' 'Gill' 'Hagadone' 'Hernandez' 'Hunt' 'King' 'Kirk' 'Larose'
-                'Licea' 'Malone' 'Martin' 'Massaro' 'Mcelvain' 'Moore' 'Paez' 'Phang'
-                'Randle' 'Read' 'Redding' 'Rich' 'Rioux' 'Rivera' 'Rodriguez' 'Rollins'
-                'Russell' 'Sawyer' 'Shaw' 'Shortridge' 'Spurbeck' 'Stumpe' 'Tiburcio'
-                'Tran' 'Vest' 'Villa' 'Waters' 'Watts']
+last_list=['Baylock', 'Benites', 'Blevins', 'Bossert', 'Bowen', 'Campbell', 'Cartwright',
+                'Cayer', 'Corradino', 'Desrochers', 'Doggett', 'Drain', 'Elizondo', 'Enos',
+                'Gardner', 'Gill', 'Hagadone', 'Hernandez', 'Hunt', 'King', 'Kirk', 'Larose',
+                'Licea', 'Malone', 'Martin', 'Massaro', 'Mcelvain', 'Moore', 'Paez', 'Phang',
+                'Randle', 'Read', 'Redding', 'Rich', 'Rioux', 'Rivera', 'Rodriguez', 'Rollins',
+                'Russell', 'Sawyer', 'Shaw', 'Shortridge', 'Spurbeck', 'Stumpe', 'Tiburcio',
+                'Tran', 'Vest', 'Villa', 'Waters', 'Watts']
 
 email_list=['gmail','hotmail','yahoo','protonmail','outlook','aol','zoho','icloud','gmx'] 
 
 # random name
 def random_name():
-    return (random.choice(first_list), random.choice(last_list))
+    first = random.choice(first_list)
+    last = random.choice(last_list)
+    return (first, last)
 
-# random generators for fake data
-def random_generator(random_list: list):
-    return "".join(random.choice(random_list))
 
 def random_email(username):
-    return f"{username}@{random_generator(email_list)}.com"
+    email = f"{username}@{random.choice(email_list)}.com"
+    return email
 
-def random_username(first,last):
-    return f"{first}_{last}{random.randint(0,100)}"
+def random_username(first, last):
+    username = f"{first}_{last}{random.randint(0,100)}"
+    return username
 
 
 

2b9d8a643f886f18f13db31289c08dc5972d8154
Anmol Singh <broanmol17@gmail.com>
2022-03-27 07:47:16 +0000

    api endpoints start

---
+++ tests/api/test_api_endpoints.py
@@ -0,0 +1,14 @@
+# starting with tests for api endpoints
+
+# the endpoints are defined according to: 
+# https://lucid.app/lucidchart/b45b7344-4270-404c-a4c0-877bf494d4cd/edit?invitationId=inv_f2d14e7e-1d22-4665-bf60-711bf47dd067&page=0_0#
+
+
+# /api/init-assessment
+# /api/verify-member
+# /api/init-check
+# /api/update
+# /api/approve-assessment
+# /api/assign-reviewers
+# /api/confirm-reviewer
+# /api/deny-reviewer

38d7037c93883fb6aaaadec5821950be8138f588
millerh1 <millerh1@livemail.uthscsa.edu>
2022-05-04 14:15:48 -0500

    Added badgr functions

---
+++ tests/utils/test_badgr_utils.py
@@ -0,0 +1,72 @@
+from app.utils import badgr_utils
+
+
+# TODO: Should this go here?
+config_test = {
+    "BADGR_USERNAME": "infrastructure@bioresnet.org",
+    "BADGR_PASSWORD": "badgrtest123",
+    "BADGR_BASE_URL": "https://api.test.badgr.com",
+    "BADGR_SCOPE": "rw:issuer rw:backpack",
+    "BADGR_ISSUER_ID": "4aQWejKFThS1NtViZk6GnQ",
+    "BADGR_GRANT_TYPE": "password",
+    "BADGR_CLIENT_ID": "public",
+    "BADGE_IDs": {
+        "Python Programming I": "OcVxPZEORASs4dBL0h5mOw",
+        "Python Programming II": "OcVxPZEORASs4dBL0h5mOw",
+        "R Programming I": "OcVxPZEORASs4dBL0h5mOw",
+        "R Programming II": "OcVxPZEORASs4dBL0h5mOw-g",
+    },
+}
+
+# TODO: Should this go here?
+# Get the bearer token
+brearer_token = badgr_utils.get_bearer_token(config_test)
+
+
+def test_issue_badge():
+    """
+    Test the issue_badge function
+    """
+
+    # Issue the badge
+    issue_badge_response = badgr_utils.issue_badge(
+        user_email="tests@bioresnet.org",
+        user_first="Test",
+        user_last="User",
+        assessment_name="Python Programming I",
+        bearer_token=brearer_token,
+        config=config_test,
+    )
+
+    # Wrangle the response
+    recip = issue_badge_response.json()["result"][0]["recipient"]
+
+    # Assert that the badge was issued correctly
+    assert issue_badge_response.status_code == 201
+    assert issue_badge_response.json()["status"]["success"]
+    assert recip["plaintextIdentity"] == "tests@bioresnet.org"
+
+
+def test_get_assertion():
+    """
+    Test the get_badge_json function
+    """
+
+    # Get the badge JSON
+    badge_assertion = badgr_utils.get_assertion(
+        assessment_name="Python Programming I",
+        user_email="tests@bioresnet.org",
+        bearer_token=brearer_token,
+        config=config_test,
+    )
+
+    # Wrangle the badge JSON
+    result = badge_assertion.json()["result"]
+    recip = result[0]["recipient"]["plaintextIdentity"]
+    assessment_name = result[0]["badgeclass"]
+
+    # Assert that the badge JSON is correct
+    assert badge_assertion.status_code == 200
+    assert badge_assertion.json()["status"]["success"]
+    assert recip == "tests@bioresnet.org"
+    assert assessment_name == config_test["BADGE_IDs"]["Python Programming I"]

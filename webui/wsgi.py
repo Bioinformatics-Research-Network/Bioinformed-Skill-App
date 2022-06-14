1f74b2bbb559d7e23672f68979eb16213de467b7
Henry Miller <millerh1@livemail.uthscsa.edu>
2022-06-14 23:44:38 +0000

    Minor update to wsgi.py

--- webui/wsgi.py
+++ webui/wsgi.py
@@ -8,8 +8,10 @@ app = create_app()
 # The docker container will forward the requests to the backend
 # Required for OAuth2 authentication to work
 # Found from reading the Flask-Dance docs
-if os.environ.get("FLASK_ENV") == "production":
+if os.environ.get("APP_ENV") == "production":
     app.wsgi_app = ProxyFix(app.wsgi_app)
+   
+
 
 if __name__ == "__main__":
     app.run()

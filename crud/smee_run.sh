2038bfeefd1a0f40fd32f0e9d1685290c96fccfc
Anmol Singh <broanmol17@gmail.com>
2023-01-23 18:27:36 +0000

    env var fixed

--- crud/smee_run.sh
+++ crud/smee_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-smee -u $GHBOT_SMEE --port 2000 &
+smee -u $CRUD_SLACK_SMEE -p 2000 -P /api/reviewer/assign_reviewer_slack &
 # Start the ghbot
 gunicorn main:app --capture-output --log-level debug --error-logfile - \
 --access-logfile - --bind :2000 --workers 2 \
